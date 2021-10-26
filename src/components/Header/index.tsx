24f650bb20f81ac48cac3c821ec8cc4d2dd40bf4
yansntss <yansantos.dev@gmail.com>
2021-10-25 22:06:55 -0300

    :sparkles: profile photoprofile photo with gradient

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-
 import { styles } from "./styles";
 
 import LogoSvg from "../../assets/logo.svg";
-import {UserPhoto} from '../UserPhoto'
+import { UserPhoto } from "../UserPhoto";
 
 export function Header() {
   return (
     <View style={styles.container}>
       <LogoSvg />
-      
-      <UserPhoto imageUri='https://github.com/yansntss.png'/> 
 
+      <TouchableOpacity>
+        <Text style={styles.logoutText}>Sair</Text>
+      </TouchableOpacity>
+
+      <UserPhoto imageUri="https://github.com/yansntss.png" />
 
       {/* qualquer coisa que tiver dentro de TouchableOpacity, torna-se clicavel*/}
-      <TouchableOpacity>
-      <Text style={styles.logoutText}>
-        Sair
-        </Text>
-      </TouchableOpacity>
     </View>
   );
 }
--- src/components/UserPhoto/styles.ts
+++ src/components/UserPhoto/styles.ts
@@ -2,10 +2,17 @@ import { StyleSheet } from 'react-native';
 import { COLORS } from '../../theme';
 
 export const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+ 
   avatar: {
-    width:48,
-    height:48,
-   
+    width: 48,
+    height: 48,
+    borderWidth: 4,
+    borderColor: COLORS.BLACK_SECONDARY
+
   },
 
 });
