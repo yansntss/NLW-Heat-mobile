2d2d141f06d0203e008910707c31f4e0aad3deed
yansntss <yansantos.dev@gmail.com>
2021-10-26 18:55:47 -0300

    :sparkles: Button log and exit

---
+++ src/components/SignInBox/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Button} from "../Button"
+import { View } from "react-native";
+
+import { styles } from "./styles";
+import { COLORS } from "../../theme";
+
+export function SignInBox() {
+  return (
+  <View style={styles.container}>
+
+    <Button 
+      title="ENTRAR COM GITHUB"
+      color={COLORS.BLACK_PRIMARY}
+      backgroundColor={COLORS.YELLOW}
+      icon='github'
+      
+
+    />
+  </View>
+    
+    );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View } from 'react-native'
 import { MessageList } from "../../components/MessageList"
-
+import { SignInBox } from "../../components/SignInBox";
 
 import { Header } from "../../components/Header";
 
@@ -11,6 +11,7 @@ export function Home(){
    <View style={styles.container}>
      <Header />
      <MessageList />
+     <SignInBox />
    </View>
   )
 }
